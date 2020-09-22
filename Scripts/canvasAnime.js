b5c5dc83c6caaf5cd831b30bddeb03c5c69bba03
Akash Gupta <akashgupta515@gmail.com>
2020-09-22 23:56:11 +0530

    new canvas design

--- Scripts/canvasAnime.js
+++ Scripts/canvasAnime.js
@@ -14,19 +14,69 @@ window.addEventListener("mousemove",function(event)
     mouse.y = event.y;
    console.log(mouse);
 });
-function LineMove(x,y)
+var colorArray = ["#334752","#46B39D","#F0CA4D","#E37332","#DE4F3C"];
+function circle (x,y,radius,dx,dy)
 {
-    c.beginPath();
-    c.moveTo(0,height);
-    c.lineTo(x,y);
-    c.strokeStyle = "white";
-    c.lineTo(width,height);
-    c.stroke();
+   this.x = x;
+   this.y = y;
+   this.radius = radius;
+   this.dx =dx;
+   this.dy=dy;
+   this.color = colorArray[Math.floor(Math.random()*5)];
+   this.minRadius = (Math.random() * 20)+2;
+   this.draw = function()
+   {
+       c.beginPath();
+       c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+       c.fillStyle = this.color;
+       c.stroke();
+       c.fill();
+   }
+   this.update = function()
+   {
+       if(this.x + this.radius >= width || this.x-this.radius < 0)
+       {
+          this.dx = -this.dx;
+       }
+       if(this.y + this.radius >= height || this.y-this.radius < 0)
+       {
+          this.dy = -this.dy;
+       }
+       this.x += this.dx;
+       this.y += this.dy;
+
+       if((mouse.x - this.x < 50 && mouse.x -this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50))
+       {
+           if(this.radius < 30)
+           {
+               this.radius += 1;
+           }
+       }else if (this.radius > this.minRadius)
+       {
+           this.radius -= 1 ;
+       }
+       this.draw();
+   }
+}
+var array = new Array();
+
+for(var i= 0 ;i<500;i++)
+{
+     var randr =   (Math.random()*20)+1;
+     var ranx  = (Math.random()*(width - randr *2))+randr;
+     var rany  = (Math.random()*(height - randr *2))+randr;
+     var randx = Math.random() * 3 +1;  
+     var randy = Math.random() * 3 +1;
+     array.push(new circle(ranx,rany,randr,randx,randy));
 }
 function animate()
 {
-    c.clearRect(0,0,width,height);
     requestAnimationFrame(animate);
-    LineMove(mouse.x,mouse.y);
+    c.clearRect(0,0,width,
+        height);
+    for(let i =0 ; i<500 ; i++)
+    {
+        array[i].update();
+    }
 }
 animate();
